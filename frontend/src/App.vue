670666da920604c3d4a01b50e0d96e47ca6d5e44
leonorader <dernori@gmail.com>
2020-07-30 07:35:12 +0200

    another karancs css

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -37,4 +37,144 @@ main {
   justify-content: center;
   align-items: center;
 }
+
+body {
+  margin: 0;
+  background: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#karancs {
+  width: 6.4cm;
+  height: 11.5cm;
+}
+
+#matrix {
+  height: 60%
+}
+
+.floor, .roof {
+  background: white;
+  width: 100%;
+  border: 1px solid black;
+  margin: -1px 0;
+  height: 12%;
+  display: flex;
+  justify-content: space-around;
+}
+
+.room {
+  height: 55%;
+  width: 12%;
+  border: 1px solid black;
+  margin: 1%;
+}
+
+.roof-hole {
+  height: 80%;
+  width: 22%;
+  border: 1px solid black;
+  margin: -1px 0;
+  margin-top: 1%;
+}
+
+
+
+
+
+#entresol {
+  height: 18%;
+  display: flex;
+  flex-direction: column;
+  align-items: center
+}
+
+#base {
+  width: 85%;
+  border: 1px solid black;
+  margin: -1px 0;
+  height: 100%;
+  background: white;
+  display: flex;
+}
+
+.tik {
+  height: 50%;
+  width: 4%;
+  margin: -1px 1%;
+  border: 1px solid black;
+  background: white;
+}
+
+.tik:first-child {
+  margin-left: -2%;
+}
+.tik:last-child {
+  margin-right: -2%;
+}
+
+.tok {
+  height: 100%;
+  width: 20%;
+  border: 1px solid black;
+  background: white;
+  margin: -1px 1%;
+
+}
+
+.tok-up {
+  height: 60%;
+  border: 1px solid black;
+  width: 100%;
+  margin: -1px 0;
+}
+
+.tok-down {
+  height: 40%;
+  border: 1px solid black;
+  width: 100%;
+  margin: -1px 0;
+}
+
+
+#hotel {
+  height: 22%;
+  display: flex;
+  flex-direction: column;
+  align-items: center
+}
+
+#line {
+  width: 100%;
+  border: 1px solid black;
+  height: 30%;
+  background: white;
+  position: relative
+}
+
+#cross {
+  width: 97%;
+  border: 1px solid black;
+  height: 70%;
+  background: white;
+  margin: -1px 0;
+  display: flex;
+  justify-content: space-around;
+}
+
+.window {
+  border: 1px solid black;
+  height: 100%;
+  width: 18%;
+}
+
+.crossline {
+  height: 5%;
+  width: 100%;
+  padding-top: 60%;
+  border: 1px solid black;
+}
+
 </style>
--- frontend/src/components/Hotel.vue
+++ frontend/src/components/Hotel.vue
@@ -1,8 +1,67 @@
 <template>
-  <main>
-    <canvas id="canvas"></canvas>
+  <!--<main>-->
+    <!--<canvas id="canvas"></canvas>-->
     <!--<p>{{hotel}}</p>-->
+  <!--</main>-->
+
+  <main id="karancs">
+    <div id="matrix" v-if="hotel">
+      <div class="roof">
+        <div class="roof-hole"></div>
+        <div class="roof-hole"></div>
+        <div class="roof-hole"></div>
+        <div class="roof-hole"></div>
+      </div>
+
+      <div v-for="i in 7" class="floor">
+        <div v-for="j in 8" class="room" :style="{'background-color': hotel[i][j].data}" :ref="'room-' + (i-1) + '-' + (j-1)"></div>
+      </div>
+    </div>
+
+    <div id="entresol">
+      <div id="base">
+        <div class="tik"></div>
+        <div class="tok" ref="window-1">
+          <div class="tok-up"></div>
+          <div class="tok-down"></div>
+        </div>
+        <div class="tik"></div>
+        <div class="tok" ref="window-2">
+          <div class="tok-up"></div>
+          <div class="tok-down"></div>
+        </div>
+        <div class="tik"></div>
+        <div class="tok" ref="window-3">
+          <div class="tok-up"></div>
+          <div class="tok-down"></div>
+        </div>
+        <div class="tik"></div>
+        <div class="tok" ref="window-4">
+          <div class="tok-up"></div>
+          <div class="tok-down"></div>
+        </div>
+        <div class="tik"></div>
+      </div>
+    </div>
+
+    <div id="hotel">
+      <div id="line"></div>
+      <div id="cross">
+        <div class="window"></div>
+        <div class="window"></div>
+        <div class="window"></div>
+        <div class="window"></div>
+        <div class="window"></div>
+      </div>
+      <div id="door"></div>
+    </div>
   </main>
+
+  <!--<main>-->
+  <!--<canvas id="canvas"></canvas>-->
+  <!--<p>{{hotel}}</p>-->
+  <!--</main>-->
+
 </template>
 
 <script>
@@ -10,12 +69,12 @@
     name: 'Hotel',
     data() {
       return {
-        canvas: null,
-        context: null,
+        // canvas: null,
+        // context: null,
 
-        unit: 16,
-        width: 2 * 16,
-        height: 16
+        // unit: 16,
+        // width: 2 * 16,
+        // height: 16
       }
     },
     computed: {
@@ -24,40 +83,39 @@
       }
     },
     methods: {
-      draw: function (frame) {
-        console.log(frame)
-        for (let m = 0; m < frame.length; m++) {
-          for (let n = 0; n < frame[m].length; n++) {
-            let x = (3 * n + 1) * this.unit
-            let y = (2 * m + 1) * this.unit
-            console.log(frame[m][n])
-            this.context.fillStyle = frame[m][n].data
-            this.context.strokeStyle = frame[m][n].data
-            this.context.fillRect(x, y, this.width, this.height)
-          }
-        }
-      }
+      // draw: function (frame) {
+      //   console.log(frame)
+      //   for (let m = 0; m < frame.length; m++) {
+      //     for (let n = 0; n < frame[m].length; n++) {
+      //       this.$refs['room-' + m + '-'+ n].
+
+
+            // let x = (3 * n + 1) * this.unit
+            // let y = (2 * m + 1) * this.unit
+            // console.log(frame[m][n])
+            // this.context.fillStyle = frame[m][n].data
+            // this.context.strokeStyle = frame[m][n].data
+            // this.context.fillRect(x, y, this.width, this.height)
+      //     }
+      //   }
+      // }
     },
     mounted() {
-      this.canvas = document.getElementById('canvas')
-      this.canvas.width = this.unit * 3 * 8 + 16
-      this.canvas.height = this.unit * 2 * 8 + 16
-
-      this.context = this.canvas.getContext('2d')
-      // this.context.fillStyle = '#fffff0'
-      // this.context.strokeStyle = '#373734'
-      this.context.lineWidth = 1
+      // this.canvas = document.getElementById('canvas')
+      // this.canvas.width = this.unit * 3 * 8 + 16
+      // this.canvas.height = this.unit * 2 * 8 + 16
+      //
+      // this.context = this.canvas.getContext('2d')
+      // this.context.lineWidth = 1
     },
     watch: {
-      hotel() {
-        console.log(this.hotel)
-        if (!this.hotel) return
-        if (this.context) this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
-
-        this.draw(this.hotel.rooms)
-        // await new Promise(resolve => setTimeout(resolve, waitTime));
-        // this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-      }
+      // hotel() {
+      //   console.log(this.hotel)
+      //   if (!this.hotel) return
+      //   if (this.context) this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
+      //
+      //   this.draw(this.hotel.rooms)
+      // }
     }
   }
 </script>
